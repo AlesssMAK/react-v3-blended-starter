5a66f226bbfccf39b6032e1cf93920d437373bb1
Alexey <makovii88@gmail.com>
2025-09-22 16:40:56 +0200

    finish commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,17 +4,105 @@ import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
 
 import css from "./App.module.css";
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../../services/postService";
+import { Post } from "../../types/post";
+import PostForm from "../CreatePostForm/CreatePostForm";
+import EditPostForm from "../EditPostForm/EditPostForm";
 
 export default function App() {
+  const [query, setQuery] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isCreatePost, setIsCreatePost] = useState<boolean>(false);
+  const [isEditPost, setIsEditPost] = useState<boolean>(false);
+  const [editPost, setEditPost] = useState<Post | null>(null);
+  const [debouncedQuery] = useDebounce(query, 300);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setIsCreatePost(false);
+    setIsEditPost(false);
+    setEditPost(null);
+  };
+
+  const createPost = () => {
+    setIsCreatePost(true);
+    setIsEditPost(false);
+  };
+
+  const disabledCreatePost = () => {
+    setIsCreatePost(false);
+  };
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["posts", debouncedQuery, page],
+    queryFn: () => fetchPosts(debouncedQuery, page),
+    placeholderData: keepPreviousData,
+  });
+
+  const totalPages = data?.totalCount ? Math.ceil(data.totalCount / 8) : 0;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    setPage(1);
+  };
+
+  const startEditPost = (postToEdit: Post) => {
+    setEditPost(postToEdit);
+    setIsEditPost(true);
+    setIsCreatePost(false);
+    openModal();
+  };
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox />
-        <Pagination />
-        <button className={css.button}>Create post</button>
+        <SearchBox value={query} onChange={onChange} />
+        {totalPages > 1 && (
+          <Pagination totalPages={totalPages} currentPage={page} onPageChange={setPage} />
+        )}
+        <button
+          className={css.button}
+          onClick={() => {
+            openModal();
+            createPost();
+          }}
+        >
+          Create post
+        </button>
       </header>
-      <Modal>{/* Передати через children компонент CreatePostForm або EditPostForm */}</Modal>
-      <PostList />
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          {isCreatePost && (
+            <PostForm
+              onClose={() => {
+                closeModal();
+                disabledCreatePost();
+              }}
+            />
+          )}
+          {isEditPost && editPost && (
+            <EditPostForm
+              initialValues={editPost}
+              onClose={() => {
+                closeModal();
+                setEditPost(null);
+              }}
+            />
+          )}
+        </Modal>
+      )}
+
+      {isSuccess && data?.posts?.length > 0 && (
+        <PostList posts={data?.posts} openModal={openModal} toggleEditPost={startEditPost} />
+      )}
     </div>
   );
 }
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,11 +1,50 @@
 import axios from "axios";
+import { Post } from "../types/post";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-export const fetchPosts = async (searchText, page) => {};
+interface fetchPostsRequest {
+  posts: Post[];
+  totalCount: number;
+}
 
-export const createPost = async (newPost) => {};
+interface NewPost {
+  title: string;
+  body: string;
+}
 
-export const editPost = async (newDataPost) => {};
+interface EditPost {
+  id: number;
+  title: string;
+  body: string;
+}
 
-export const deletePost = async (postId) => {};
+export const fetchPosts = async (searchText: string, page: number): Promise<fetchPostsRequest> => {
+  const params = {
+    q: searchText,
+    _page: page,
+    _limit: 8,
+  };
+
+  const res = await axios.get<Post[]>("/posts", { params });
+  console.log(res);
+
+  const totalCount = Number(res.headers["x-total-count"]);
+
+  return { posts: res.data, totalCount };
+};
+
+export const createPost = async (newPost: NewPost) => {
+  const { data } = await axios.post<Post>("/posts", newPost);
+  return data;
+};
+
+export const editPost = async (newDataPost: EditPost) => {
+  const { data } = await axios.patch<Post>(`/posts/${newDataPost.id}`, newDataPost);
+  return data;
+};
+
+export const deletePost = async (postId: number) => {
+  const { data } = await axios.delete<Post>(`/posts/${postId}`);
+  return data;
+};
